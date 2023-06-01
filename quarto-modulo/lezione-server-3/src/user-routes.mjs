cc46d1d19d08737776bc195a9bdb66faf7405811
salvatore <salvatore.tosich.dev@gmail.com>
2023-06-01 17:00:49 +0200

    pomeriggio di giovedi

--- quarto-modulo/lezione-server-3/src/user-routes.mjs
+++ quarto-modulo/lezione-server-3/src/user-routes.mjs
@@ -1,14 +1,21 @@
 import fs from 'node:fs/promises'
 import users from '../db/users.json' assert { type: 'json' }
-
+import usersSign from '../db/users-registred.json' assert { type: 'json' }
 import axios from "axios"
 
 const DB_PATH = './db/users.json'
+const DB_PATH_SIGN = './db/users-registred.json'
 
 const myServerKey = "Salvo"
 
 let NEXT = Object
   .keys(users)
+  .reduce((biggest, id) => {
+    return biggest > parseInt(id, 10) ? biggest : parseInt(id, 10)
+  }, 0)
+
+let IdSign = Object
+  .keys(usersSign)
   .reduce((biggest, id) => {
     return biggest > parseInt(id, 10) ? biggest : parseInt(id, 10)
   }, 0)
@@ -31,7 +38,7 @@ export const create = async (req, res) => {
       return {}
     }
   }
-  console.log(req.headers);
+  //console.log(req.headers);
   let result = await callUser(NEXT)
   users[NEXT] = { ...req.body, ...result }
 
@@ -76,7 +83,7 @@ export const search = (req, res) => {
 export const update = async (req, res) => {
 
   if (req.headers.authorization === myServerKey) {
-      let user = users[req.params.id]
+    let user = users[req.params.id]
     if (user) {
       let newUser = { ...user, ...req.body }
       users[req.params.id] = newUser
@@ -91,25 +98,24 @@ export const update = async (req, res) => {
           message: 'user not found'
         })
     }
-  } else{
+  } else {
     res
-    .status(401)
-    .send({
-      data: {},
-          error: true,
-          message: "Sorry, you don't have authorization"
-    })
+      .status(401)
+      .send({
+        data: {},
+        error: true,
+        message: "Sorry, you don't have authorization"
+      })
   }
-  
+
 }
 
 export const remove = async (req, res) => {
 
-
   //console.log(req.headers.authorization);
 
   if (req.headers.authorization === myServerKey) {
-      let user = users[req.params.id]
+    let user = users[req.params.id]
     if (user) {
       delete users[req.params.id]
       await fs.writeFile(DB_PATH, JSON.stringify(users, null, '  '))
@@ -124,14 +130,99 @@ export const remove = async (req, res) => {
         })
     }
 
-  }else{
+  } else {
     res
-    .status(401)
-    .send({
-      data: {},
-          error: true,
-          message: "Sorry, you don't have authorization"
-    })
+      .status(401)
+      .send({
+        data: {},
+        error: true,
+        message: "Sorry, you don't have authorization"
+      })
   }
-  
+
 }
+
+/// esercizio 2
+
+export const signIn = async (req, res) => {
+  IdSign++
+
+  // DB_PATH_SIGN
+
+  // usersSign // nome oggetto
+
+  // abbiamo un id di riferimento
+  // dobbiamo tener conto dello stesso username se e uguale restituire un messaggio che specifica il problema
+
+  // se l'utente e gia registrato? 
+
+  //console.table(Object.values(usersSign)[0].username);
+
+  let UsersNames = Object.values(usersSign)
+
+  console.log("\n" + UsersNames + "\n");
+
+  if (UsersNames.length > 0) {
+    for (let i = 0; i < UsersNames.length; i++) {
+      const usName = UsersNames[i].username
+      // console.log("\n" + usName + "\n");
+      // console.log("\n" + typeof usName + "\n");
+      // console.log("\n" + req.body + "\n");
+
+      if (!usName == req.body.username) {
+        usersSign[IdSign] = { ...req.body }
+        res
+        .status(200)
+        .send({
+          message : "utente creato"
+        })
+        await fs.writeFile(DB_PATH_SIGN, JSON.stringify(usersSign, null, '  '))
+      }
+    }
+    console.log(usersSign[IdSign]);
+
+    res
+      .send(UsersNames)
+      .status(203)
+  // } else{
+  //   usersSign[IdSign] = { ...req.body }
+  //   res
+  //   .status(202)
+  //   .send({
+  //     message : "utente creato"
+  //   })
+
+  //   await fs.writeFile(DB_PATH_SIGN, JSON.stringify(usersSign, null, '  '))
+  // }
+}
+
+// DB_PATH_SIGN
+
+  // usersSign // nome oggetto
+
+  // export const logIn = async (req, res) => {
+
+  //   let query = req.query
+
+  //   let filter = Object.values(usersSign)
+  //     .filter(n =>n.username === query.username && n.password === query.password)
+
+  //     console.log(filter);
+
+  //     if (filter.length>0) {
+  //       res
+  //       .status(200)
+  //       .send(
+  //         filter
+  //       )
+  //     } else {
+  //       res
+  //       .status(200)
+  //       .send({
+  //         message: "immetti username o password corretti"
+  //       })
+  //     }
+      
+
+
+  // } 
