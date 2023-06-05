d086b8d45826fc8ba01bf178935411457bfa26aa
salvatore <salvatore.tosich.dev@gmail.com>
2023-06-05 15:41:35 +0200

    inizio esercizio

---
+++ quarto-modulo/lezione-server-4/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express'  // qui si importa la libreria express, si usa una sintassi diversa rispetto alla default, (require) vedi jsonpack si inseririsce il type module
+const app = express()
+const port = 3000
+
+import bodyParser from 'body-parser' // si importa questa libreria per parsare le stringhe nei post e nei put, si installa poi in terminale npm install body-parser
+app.use(bodyParser.json()) //va insieme al precedente
+
+
+import * as user from './user-routes.mjs' // si importano tutti gli elementi dal path e si da loro il nome user attraverso "as"
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`) // stringhe di default che associano il temrminale alla portasopra indicata all'inizio pagina
+})
